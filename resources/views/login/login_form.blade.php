2d4a471aa8bb51ec69e255476b3f6821438e05a8
TakatsuguSeki <t.seki@ebacorp.jp>
2021-10-20 14:47:34 +0900

    ログインフォーム作成、バリデーション設定

---
+++ resources/views/login/login_form.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BREMEN　管理ログイン画面</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/signin.css') }}" rel="stylesheet">
+</head>
+<body>
+    <form class="form-signin" method="post" action="{{ route('login') }}">
+        @csrf
+        <h1 class="h3 mb-3 font-weight-normal">BREMEN<br>管理ログイン画面</h1>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <label for="inputId" class="sr-only">Login ID</label>
+        <input type="text" id="inputid" name="id" class="form-control" placeholder="ログインID" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">認証</button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2021 ebacorp.inc</p>
+    </form>
+</body>
+</html>
